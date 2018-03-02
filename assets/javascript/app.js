5e66345d5fa407218e2d3cb586963e97805d0caf
Ryan Mullowney <Batman@Ryans-MacBook-Pro.local>
2018-03-02 16:33:58 -0600

    Adds a score counter.  Puts the on-click functionality inside its own function that is called when an answer is clicked. Will be adding loop to cycle through questions.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@
     var questionBank = [
 
         question1 = {
-            question: "This is question 2",
+            question: "This is question 1",
             answer1: 'answer1.1',
             answer2: 'answer1.2',
             answer3: 'answer1.3',
@@ -36,8 +36,18 @@
         }
     ]
 
-    // Timer variable and countdown
+    // Define timer & score variables
     var timer = 10;
+    var score = 0;
+
+    // Track index of questionBank
+    var currentQuestion = 0
+
+    // Define timeoutVar/intervalVar on the outer scope so they can be used in 'decrement()' and 'buttonClicked()' functions
+    var intervalVar;
+    var timeoutVar;
+    // Define betweenQuestionsTimer for the timer before the next question is shown
+    var betweenQuestionsTimer;
 
 
     // Function to decrease the timer on the page
@@ -52,10 +62,6 @@
     }
 
 
-    // Track index of questionBank
-    var i = 0
-
-
     // Function to display the question on the page
     function displayQuestion(i) {
         $('#timeRemaining').html("<h3 id='timeRemaining'> Time remaining: " + timer + '</h3>');
@@ -64,24 +70,43 @@
         $('#gameContent').append("<button class='btn btn-primary' id='wrong'>" + questionBank[i].answer2 + "</button>");
         $('#gameContent').append("<button class='btn btn-primary' id='wrong'>" + questionBank[i].answer3 + "</button>");
         $('#gameContent').append("<button class='btn btn-primary' id='correct'>" + questionBank[i].correctAnswer + "</button>");
-
+        $('#playerScore').append("Your score: " + score)
     }
+
 
     // Present information if the timer runs out (probably adjust score variables and such here too since the player won't get points)
     function timeUp() {
         $('#timeRemaining').html("<h3 id='timeRemaining'>Time's Up!</h3>");
         $('#gameContent').html("Info about the question")
+        currentQuestion++;
     }
 
-    // Define intervalVar on the outer scope so it can be used in 'decrement' function
-    var intervalVar;
+    // Function to determine what happens when a button is clicked
+    // Passes 'this' of the button clicked into the function under the variable 'clicked'
+    function buttonClicked(clicked) {
+        clearTimeout(timeoutVar);
+        clearInterval(intervalVar);
+
+        // 'this' below is referencing the window.  Gotta figure out how to make it read the button click.
+        if ($(clicked).attr('id') === 'wrong') {
+            $('#timeRemaining').html("<h3 id='timeRemaining'>Your guess is wrong</h3>");
+            $('#gameContent').html("Info about the question");
+        } else {
+            $('#timeRemaining').html("<h3 id='timeRemaining'>Your guess is right</h3>");
+            $('#gameContent').html("Info about the question");
+            score++;
+            $('#playerScore').html("Your score: " + score++);
+        }
+        currentQuestion++;
+    }
+
 
     // Function to initialize the game once the startGame button is clicked
     function initGame() {
 
         // Variables for my timers so I can disable them on certain conditions (intervalVar defined on outer scope above)
         intervalVar = setInterval(decrement, 1000);
-        var timeoutVar = setTimeout(timeUp, 1000 * 10);
+        timeoutVar = setTimeout(timeUp, 1000 * 10);
 
         // Remove the 'Click button to begin!' text
         $('h4').remove();
@@ -90,21 +115,13 @@
         $('#startGame').remove();
 
         // Present the question on the page
-        displayQuestion(i);
+        displayQuestion(currentQuestion);
 
         intervalVar;
         timeoutVar;
 
         $(".btn").on('click', function () {
-            clearTimeout(timeoutVar);
-            clearInterval(intervalVar);
-            if ($(this).attr('id') === 'wrong') {
-                $('#timeRemaining').html("<h3 id='timeRemaining'>Your guess is wrong</h3>");
-                $('#gameContent').html("You clicked a button for the question");
-            } else {
-                $('#timeRemaining').html("<h3 id='timeRemaining'>Your guess is right</h3>");
-                $('#gameContent').html("You clicked a button for the question");
-            }
+            buttonClicked(this);
         })
     }
 
